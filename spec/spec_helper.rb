0f91bd3bc6f108daf206443c58b13b6603a1e882
Jamie Mill <jamiermill@gmail.com>
2011-01-28 09:07:33 +0000

    Updated for rails 3 and rspec 2 compatibility.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,14 @@
 require 'rubygems'
-gem 'activerecord', '>= 1.15.4.7794'
+gem 'activerecord', '>= 3'
 require 'active_record'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'acts_as_restful_list'
-require 'spec'
-require 'spec/autorun'
+require 'rspec'
+require 'rspec/autorun'
 
 ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
 end
